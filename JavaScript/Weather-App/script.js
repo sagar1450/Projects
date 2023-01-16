4f60ccf9c8ad8d86545c0c67866fd23a8fa28399
Sagar Rathod <sagarrathod539@gmail.com>
2023-01-16 19:16:29 +0530

    add more weather information

--- JavaScript/Weather-App/script.js
+++ JavaScript/Weather-App/script.js
@@ -1,62 +1,67 @@
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'cdd527cb90msh9828515f57b6cb5p1afb5ajsnc0fbc4bffbc1',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'cdd527cb90msh9828515f57b6cb5p1afb5ajsnc0fbc4bffbc1',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
 };
-const getWeather=city=>{   
-    console.log(city)
-    cityName.innerHTML=city;
-    
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
-	.then(response => response.json())
-	.then(response => {
-       
-         temp.innerHTML=response.temp +"℃";
-                
-    })
 
-   
-	.catch(err => console.error(err));
-     //return showWeather(city)
-   
+const getWeather = city => {
+
+    cityName.innerHTML = city;
+
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+
+            temp.innerHTML = response.temp + "℃";
+            feels_like.innerHTML = response.feels_like + "℃";
+
+        })
+
+        .catch(err => console.error(err));
+              
+
 }
 
-submit.addEventListener("click",(e)=>{
+submit.addEventListener("click", (e) => {
     e.preventDefault();
     getWeather(city.value);
+    city.value="";
 })
 
-const showWeather=(city)=>{
-city=cityName.textContent;
+const showWeather = (city) => {
+    city = cityName.textContent;
     fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
-	.then(response => response.json())
-	.then(response => {console.log(response)
-        ul.innerHTML=`
+        .then(response => response.json())
+        .then(response => {
+
+            container.innerHTML += ` <div id="info" class="main"> 
+        <ul id="ul" class="list-unstyled mt-3 mb-4"> 
         <li>Cloud pct : <span id="cloud_pct">${response.cloud_pct}</span></li>
         <li>Humidity : <span id="humidity">${response.humidity}</span></li>       
-        <li>Min-Temp : <span id="min_temp">${response.min_temp}</span></li>
-        <li>Max-Temp : <span id="max_temp">${response.max_temp}</span></li>`;
+        <li>wind_degrees : <span id="wind_degrees">${response.wind_degrees}</span></li>
+        <li>wind_speed : <span id="wind_speed">${response.wind_speed}</span></li>
+        </ul>
+        <button id="btn1" class="btn1">Close</button>
+        </div>`;
 
-     })
-      
-     
-   
-	.catch(err => console.error(err));
+        })
 
-
-        main.removeChild(btn);
-   
+        .catch(err => console.error(err));    
 }
 
-btn.addEventListener('click',e=>{
-    // e.preventDefault();
-    //getWeather(city.value);  
-     showWeather(city.value)           
 
-      
- 
+container.addEventListener('click',e=>{
+    if(e.target.classList.contains("btn1")){
+        container.removeChild(info);
+    }
+    if(e.target.classList.contains("btn")){
+        e.preventDefault();   
+        showWeather(city.value)
+    }
 })
 
 getWeather("Delhi")
+
